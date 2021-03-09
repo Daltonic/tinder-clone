dffd118080f4366985092e412a58e6a9752c10df
Daltonic <darlingtongospel@gmail.com>
2021-03-09 07:04:46 +0100

    Feat: Adds the main header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainHeader from './components/MainHeader'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    MainHeader
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
 }
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="main-header">
+        <h4>This is the main header</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "main-header"
+    }
+</script>
+
+<style scoped>
+
+</style>
