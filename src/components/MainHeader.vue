a9b85f18605da74c1453ba0322042912138b0608
Daltonic <darlingtongospel@gmail.com>
2021-03-09 08:20:23 +0100

    Feat: Add styles and icons to the main header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,15 +1,42 @@
 <template>
-    <div id="main-header">
-        <h4>This is the main header</h4>
-    </div>
+  <div id="main-header">
+    <AccountIcon :size="40" class="header__icon"/>
+    <FireIcon fillColor="#fd5068" :size="40" class="header__icon"/>
+    <ForumIcon :size="40" class="header__icon"/>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "main-header"
-    }
+import ForumIcon from 'vue-material-design-icons/Forum.vue'
+import AccountIcon from 'vue-material-design-icons/Account.vue'
+import FireIcon from 'vue-material-design-icons/Fire.vue'
+export default {
+  name: "main-header",
+  components: {
+      ForumIcon,
+      AccountIcon,
+      FireIcon,
+  }
+};
 </script>
 
 <style scoped>
+#main-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    z-index: 100;
+    border-bottom: 1px solid #f9f9f9;
+}
+.header__icon {
+    cursor: pointer;
+    padding: 20px;
+    border-radius: 15px;
+    background: transparent;
+}
+.header__icon:hover {
+    transform: scale(1.06);
+    transition: all 0.2s ease-in-out;
+}
 
 </style>
