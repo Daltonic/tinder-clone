2e5d2440b62161d54d9e9d920c8735d2c23e6614
Daltonic <darlingtongospel@gmail.com>
2021-03-10 17:04:16 +0100

    Configures the init of comet chat

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import { CometChat } from '@cometchat-pro/chat'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const appID = process.env.VUE_APP_ID
+const region = process.env.VUE_APP_REGION
+const appSetting = new CometChat.AppSettingsBuilder()
+  .subscribePresenceForAllUsers()
+  .setRegion(region)
+  .build()
+
+CometChat.init(appID, appSetting).then(
+  () => {
+    console.log('Initialization completed successfully')
+    // You can now call login function.
+    new Vue({
+      render: (h) => h(App),
+    }).$mount('#app')
+  },
+  (error) => {
+    console.log('Initialization failed with error:', error)
+    // Check the reason for error and take appropriate action.
+  }
+)
