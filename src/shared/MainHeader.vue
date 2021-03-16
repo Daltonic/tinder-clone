892dc6205bc416b2a709df44f55a5f9f8e5ab5b5
Daltonic <darlingtongospel@gmail.com>
2021-03-16 23:06:26 +0100

    Makes mobile navigation responsive on home screen

--- src/shared/MainHeader.vue
+++ src/shared/MainHeader.vue
@@ -1,15 +1,29 @@
 <template>
   <div id="main-header">
-    <FireIcon fillColor="#fd5068" :size="40" class="header__icon" />
+    <router-link class="header__icon mobile--only" to="/friends">
+      <AccountGroupIcon fillColor="#989898" :size="30" />
+    </router-link>
+    
+    <router-link class="header__icon" to="/">
+      <FireIcon fillColor="#fd5068" :size="40" />
+    </router-link>
+
+    <router-link class="header__icon mobile--only" to="/profile">
+      <AccountIcon fillColor="#989898" :size="30" />
+    </router-link>
   </div>
 </template>
 
 <script>
+import AccountGroupIcon from "vue-material-design-icons/AccountGroup.vue";
 import FireIcon from "vue-material-design-icons/Fire.vue";
+import AccountIcon from "vue-material-design-icons/Account.vue";
 export default {
   name: "main-header",
   components: {
+    AccountGroupIcon,
     FireIcon,
+    AccountIcon,
   },
 };
 </script>
@@ -18,9 +32,10 @@ export default {
 #main-header {
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   z-index: 100;
-  border-bottom: 1px solid #f9f9f9;
+  border-bottom: 1px solid #f5f0f0c2;
+  padding: 2px;
 }
 .header__icon {
   cursor: pointer;
@@ -31,5 +46,11 @@ export default {
 .header__icon:hover {
   transform: scale(1.06);
   transition: all 0.2s ease-in-out;
+}
+
+@media only screen and (min-width: 768px) {
+  .mobile--only {
+    display: none;
+  }
 }
 </style>
